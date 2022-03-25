5a08ecbd852d548987ef236f3f6a19d3449d053a
Eric D Hiller <eric@hiller.pro>
2022-03-25 08:07:21 -0500

    Working crosshairs & BarMargin

--- AvaloniaTest/ViewModels/LiveChartsSampleViewModel.cs
+++ AvaloniaTest/ViewModels/LiveChartsSampleViewModel.cs
@@ -3,15 +3,17 @@ using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using LiveChartsCore;
 using LiveChartsCore.Defaults;
+using LiveChartsCore.Drawing;
 using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Drawing;
 using LiveChartsCore.SkiaSharpView.Painting;
 using LiveChartsCore.SkiaSharpView.Painting.Effects;
 using SkiaSharp;
 
 namespace AvaloniaTest.ViewModels;
 
-public class LiveChartsSampleViewModel : ViewModelBase {
-
+public class LiveChartsSampleViewModel : ViewModelBase
+{
     private const int _tasks = 10;
 
     private const int _delay = 100;
@@ -33,43 +35,46 @@ public class LiveChartsSampleViewModel : ViewModelBase {
 
     private          Random                               _rgen          = new Random();
     private          DateTime                             _dtBase        = new DateTime(2021, 10, 01);
-    private        static  int                                  _minuteCounter = 0;
+    private static   int                                  _minuteCounter = 0;
     private readonly ObservableCollection<FinancialPoint> _values;
     // private static   FinancialPoint                       s_current;
 
 
-    public Axis[] XAxes { get; set; } = new[]
-    {
+    public Axis[] XAxes { get; set; } = new[] {
         // set the UnitWidth to "days" to support date time scaled points.
-        new Axis
-        {
-            UnitWidth = TimeSpan.FromMinutes(1).Ticks, LabelsRotation = 80, Labeler = p => new DateTime((long)p).ToShortTimeString(),
-            // MinStep        = TimeSpan.FromMinutes(1).Ticks
+        new Axis {
+            UnitWidth      = TimeSpan.FromMinutes(1).Ticks,
+            LabelsRotation = 80,
+            Labeler        = p => new DateTime((long)p).ToShortTimeString(),
+            MinStep = TimeSpan.FromMinutes(1).Ticks
+            // MinStep        = TimeSpan.FromMinutes(1).Ticks,
+            // Padding = new Padding(50)
+            // Padding =  new Padding { Bottom = 80, Left = 80, Right = 80, Top = 80 }
         }
     };
 
-    private FinancialPoint CreateFinancialPoint() => new FinancialPoint(
-        _dtBase.AddMinutes(_minuteCounter++),
-        _rgen.Next(75, 150),
-        _rgen.Next(20, 150),
-        _rgen.Next(20, 74),
-        _rgen.Next(20, 150)
-    );
-
+    private FinancialPoint CreateFinancialPoint() {
+        var high = _rgen.Next(75, 150);
+        var low  = _rgen.Next(20, 75);
+        return new FinancialPoint(
+            _dtBase.AddMinutes(_minuteCounter++),
+            high,
+            _rgen.Next(low, high),
+            _rgen.Next(low, high),
+            low
+        );
+    }
 
 
     public ObservableCollection<RectangularSection> Sections { get; set; }
-        = new ObservableCollection<RectangularSection>
-        {
-            new RectangularSection
-            {
-                Yi = 25,
-                Yj = 25,
-                Stroke = new SolidColorPaint
-                {
-                    Color = SKColors.Red,
+        = new ObservableCollection<RectangularSection> {
+            new RectangularSection {
+                Xi = 25,
+                Xj = 25,
+                Stroke = new SolidColorPaint {
+                    Color           = SKColors.DarkGray,
                     StrokeThickness = 3,
-                    PathEffect = new DashEffect(new float[] { 6, 6 })
+                    PathEffect      = new DashEffect(new float[] { 6, 6 })
                 }
             },
         };
@@ -77,23 +82,52 @@ public class LiveChartsSampleViewModel : ViewModelBase {
     public ISeries[] Series { get; set; }
 
     private readonly Action<Action> _uiThreadInvoker;
-    public LiveChartsSampleViewModel(Action<Action> uiThreadInvoker)
-    {
-        
+
+    public LiveChartsSampleViewModel(Action<Action> uiThreadInvoker) {
         Console.WriteLine("Constructed.");
         _values = new ObservableCollection<FinancialPoint>();
-        for (int i = 0; i < 100; i++)
-        {
+        for (int i = 0; i < 100; i++) {
             _values.Add(CreateFinancialPoint());
-            Console.WriteLine($"{_values[^1].Date}\t{_values[^1].Open}\t{_values[^1].High}\t{_values[^1].Low}\t{_values[^1].Close}");
+            // Console.WriteLine($"{_values[^1].Date}\t{_values[^1].Open}\t{_values[^1].High}\t{_values[^1].Low}\t{_values[^1].Close}");
         }
 
-        Series = new ISeries[]
-        {
-            new CandlesticksSeries<FinancialPoint>
-            {
-                Values = _values,
-                MaxBarWidth = 2,
+        var                             defaultSeries = new CandlesticksSeries<FinancialPoint>();
+        if (LiveCharts.CurrentSettings.GetTheme<SkiaSharpDrawingContext>().SeriesDefaultsResolver is { } resolver) {
+            resolver(new LvcColor[] { new LvcColor() }, defaultSeries, false);
+        }
+
+        SolidColorPaint upStroke = new SolidColorPaint((defaultSeries.UpStroke as SolidColorPaint)?.Color ?? SKColors.Green); 
+        // {
+            // StrokeThickness = -5
+        // };
+        // SolidColorPaint upStroke = new SolidColorPaint(SKColors.Orange);
+        // {
+            // StrokeMiter = 0,
+            // StrokeThickness = -5
+        // };
+        // SolidColorPaint? upStroke = null;
+        SolidColorPaint downStroke = new SolidColorPaint((defaultSeries.DownStroke as SolidColorPaint)?.Color ?? SKColors.Red);
+        SolidColorPaint upFill     = new SolidColorPaint((defaultSeries.UpFill as SolidColorPaint)?.Color     ?? SKColors.Green);
+        // {
+        // SolidColorPaint upFill = new SolidColorPaint(SKColors.Purple) {
+        // StrokeThickness = -5,
+        // };
+        SolidColorPaint downFill   = new SolidColorPaint((defaultSeries.DownFill as SolidColorPaint)?.Color   ?? SKColors.Red);
+
+        Series = new ISeries[] {
+            new CandlesticksSeries<FinancialPoint> {
+                Values      = _values,
+                MaxBarWidth = 10000,
+                // DataPadding = new LvcPoint(0.75f, 0),
+                // MaxBarWidth = 
+                // UpStroke   = new SolidColorPaint(new SKColor(255, 250, 250, 250)),
+                UpStroke = upStroke,
+                // DownStroke =  new SolidColorPaint(new SKColor(255, 250, 250, 250)),
+                DownStroke = downStroke,
+                UpFill     = upFill,
+                DownFill   = downFill,
+                BarMargin = 0.75f,
+                // AnimationsSpeed = null
                 // EasingFunction = null,
                 // AnimationsSpeed = TimeSpan.FromSeconds(1),
                 // MaxBarWidth = TimeSpan.FromMinutes(1).Ticks * 0.25
@@ -106,41 +140,38 @@ public class LiveChartsSampleViewModel : ViewModelBase {
 
         return; // KILL
         // create {readTasks} parallel tasks that will add a point every {_delay} milliseconds
-        for (var i = 0; i < _tasks; i++)
-        {
+        for (var i = 0; i < _tasks; i++) {
             ReadData();
         }
     }
 
     public object Sync { get; } = new object();
 
-    private async void ReadData()
-    {
+    private async void ReadData() {
         await Task.Delay(1000);
         int count = 0;
 
-        while (true)
-        {
+        while (true) {
             await Task.Delay(_delay);
-            
+
             // force the change to happen in the UI thread.
-            _uiThreadInvoker(() =>
-            {
+            _uiThreadInvoker(() => {
                 // if (count++ > _updateLoops)
                 // {
-                    _values.Add(CreateFinancialPoint());
-                    _values.RemoveAt(0);
-                    // count = 0;
+                _values.Add(CreateFinancialPoint());
+                _values.RemoveAt(0);
+                // count = 0;
                 // }
                 // else
                 // {
-                    // Console.WriteLine($"was {_values[^1].High}");
-                    // _values[^1].High = _values[^1].High + 1;
-                    // Console.WriteLine($"updated to {_values[^1].High}");
+                // Console.WriteLine($"was {_values[^1].High}");
+                // _values[^1].High = _values[^1].High + 1;
+                // Console.WriteLine($"updated to {_values[^1].High}");
                 // }
             });
         }
     }
+
     #endregion
 }
 
--- AvaloniaTest/Views/LiveChartsSampleView.axaml.cs
+++ AvaloniaTest/Views/LiveChartsSampleView.axaml.cs
@@ -1,5 +1,6 @@
 using System;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Avalonia.Threading;
 using AvaloniaTest.ViewModels;
@@ -13,114 +14,84 @@ using SkiaSharp;
 
 namespace AvaloniaTest.Views;
 
-public partial class LiveChartsSampleView : UserControl {
-
+public partial class LiveChartsSampleView : UserControl
+{
     // private Section SectionLineX;
-    public LiveChartsSampleView()
-    {
+    public LiveChartsSampleView() {
         Console.WriteLine("hello");
         InitializeComponent();
-            
+
         var viewModel = new LiveChartsSampleViewModel(InvokeOnUIThread);
         DataContext = viewModel;
     }
 
 
-    private void ChartPointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
-    {
-        var chart = this.FindControl<CartesianChart>("chart");
-        var viewModel = DataContext as LiveChartsSampleViewModel;
-
-        Console.WriteLine( viewModel.Sections);
-        Console.WriteLine( $"Sections: {viewModel.Sections.Count}" );
-        Console.WriteLine($"Chart is " + (chart is null ? "null" : chart.Name));
-
-        // gets the point in the UI coordinates.
-        var p = e.GetPosition(chart);
-        // var cp = e.GetCurrentPoint(chart);
-
-        var scaledPoint = chart.ScaleUIPoint(new LvcPoint((float)p.X, (float)p.Y));
-
-        Console.WriteLine($"Position is {p}");
-        Console.WriteLine($"Position on Chart is len={scaledPoint.Length} {scaledPoint[0]}, {scaledPoint[1]}");
-
-        // // scales the UI coordintaes to the corresponging data in the chart.
-        // // ScaleUIPoint retuns an array of double
-        // var scaledPoint = chart.ScaleUIPoint(new LvcPoint((float)p.X, (float)p.Y));
-
-        // // where the X coordinate is in the first position
-        // var x = scaledPoint[0];
-
-        // // and the Y coordinate in the second position
-        // var y = scaledPoint[1];
-
-        // finally add the new point to the data in our chart.
-        // viewModel.Sections[0].Xi = 148;
-        // viewModel.Sections[0].Xj = 148;
-
-        viewModel.Sections[0] = new RectangularSection
-        {
-            Yi = scaledPoint[1],
-            Yj = scaledPoint[1],
-            Stroke = new SolidColorPaint
-            {
-                Color = SKColors.Purple,
-                StrokeThickness = 3,
-                PathEffect = new DashEffect(new float[] { 6, 6 })
-            }
-        };
-
-         
-    }
+    // private void ChartPointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+    // {
+    //     var chart     = this.FindControl<CartesianChart>("OhlcChart") ?? throw new NullReferenceException();
+    //     var viewModel = DataContext as LiveChartsSampleViewModel      ?? throw new NullReferenceException();
+    //
+    //     Console.WriteLine( viewModel.Sections);
+    //     Console.WriteLine( $"Sections: {viewModel.Sections.Count}" );
+    //
+    //     // gets the point in the UI coordinates.
+    //     var p = e.GetPosition(chart);
+    //     // var cp = e.GetCurrentPoint(chart);
+    //
+    //     var scaledPoint = chart.ScaleUIPoint(new LvcPoint((float)p.X, (float)p.Y));
+    //
+    //     Console.WriteLine($"Position is {p}");
+    //     Console.WriteLine($"Position on Chart is len={scaledPoint.Length} {scaledPoint[0]}, {scaledPoint[1]}");
+    //
+    //     viewModel.Sections[0] = new RectangularSection
+    //     {
+    //         Yi = scaledPoint[1],
+    //         Yj = scaledPoint[1],
+    //         Stroke = new SolidColorPaint
+    //         {
+    //             Color = SKColors.Purple,
+    //             StrokeThickness = 3,
+    //             PathEffect = new DashEffect(new float[] { 6, 6 })
+    //         }
+    //     };
+    // }
 
 
-    private void ChartPointerMoved(object? sender, Avalonia.Input.PointerEventArgs e)
-    {
-        var chart = this.FindControl<CartesianChart>("chart");
-        var viewModel = DataContext as LiveChartsSampleViewModel;
+    private void ChartPointerMoved(object? sender, Avalonia.Input.PointerEventArgs e) {
+        var chart = this.FindControl<CartesianChart>("OhlcChart") ?? throw new NullReferenceException();
+        chart.Cursor = new Cursor(StandardCursorType.Arrow);
+        var viewModel = DataContext as LiveChartsSampleViewModel ?? throw new NullReferenceException();
 
-        var p = e.GetPosition(chart);
+        var p           = e.GetPosition(chart);
         var scaledPoint = chart.ScaleUIPoint(new LvcPoint((float)p.X, (float)p.Y));
 
         Console.WriteLine($"Point Position is {p}; Position on Chart is len={scaledPoint.Length} {scaledPoint[0]}, {scaledPoint[1]}");
 
-        viewModel.Sections[0] = new RectangularSection
-        {
-            Yi = scaledPoint[1],
-            Yj = scaledPoint[1],
-            Stroke = new SolidColorPaint
-            {
-                Color = SKColors.Purple,
+        viewModel.Sections[0] = new RectangularSection {
+            Xi = scaledPoint[0],
+            Xj = scaledPoint[0],
+            Stroke = new SolidColorPaint {
+                Color           = SKColors.DarkGray,
                 StrokeThickness = 3,
-                PathEffect = new DashEffect(new float[] { 6, 6 })
+                PathEffect      = new DashEffect(new float[] { 6, 6 })
             }
         };
-
-
     }
 
-    
-        
-        
+
 // this method takes another function as an argument.
 // the idea is that we are invoking the passed action in the UI thread
 // but the UI framework will let the view model how to do this.
 // we will pass the InvokeOnUIThread method to our view model so the view model knows how
 // to invoke an action in the UI thread.
-    private void InvokeOnUIThread(Action action)
-    {
+    private void InvokeOnUIThread(Action action) {
         Dispatcher.UIThread.Post(action);
     }
-    
-    
-    private void InitializeComponent()
-    {
+
+
+    private void InitializeComponent() {
         AvaloniaXamlLoader.Load(this);
     }
 
-    public void OnPointerMove( LvcPoint point ) {
-        
-    }
-    
-    
+    public void OnPointerMove(LvcPoint point) { }
 }
